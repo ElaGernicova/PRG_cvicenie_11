44ac7c4051a77de302ce3462c359f86d645ba054
Ela Gernicova <283249@vutbr.cz>
2026-04-22 09:06:46 +0200

    :)

---
+++ sorting.py
@@ -0,0 +1,29 @@
+import random
+
+def random_numbers(count, low=0, high=100):
+    return [random.randint(low, high) for _ in range(count)]
+
+
+def selection_sort(sequence):
+    sequence = sequence[:]
+    n = len(sequence)
+    for it in range(n):
+        min_index = it
+        for index in range(it + 1,n):
+            if sequence[index] < sequence[min_index]:
+                min_index = index
+        sequence[it], sequence[min_index] = sequence[min_index], sequence[it]
+
+    return sequence
+
+
+def main():
+    sequence = random_numbers(20,0,100)
+    sorted_sequence = selection_sort(sequence)
+    a = selection_sort(sequence)
+    print(sequence)
+    print(a)
+
+
+if __name__ == "__main__":
+    main()
